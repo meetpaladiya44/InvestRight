5f6fee62d8902cc5520e11fa6569c40eef56debd
abhi152003 <abhiprajapati011@gmail.com>
2024-08-09 23:26:01 +0530

    changes in form

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -438,7 +438,7 @@ const Hero = ({ titleData, createPrediction }) => {
                     )} */}
                     <button
                       type="submit"
-                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none  transition duration-200 rounded shadow-md newColor"
+                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none  transition duration-200 rounded shadow-md newColor"
                       onClick={(e) => handleCreatePrediction(e)}
                     >
                       Make Prediction
