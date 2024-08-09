c9be4a1097587e5217355d6f31222f7cf9b49732
meetpaladiya44 <meetpaladiya2178@gmail.com>
2024-08-09 16:48:23 +0530

    Made changes in navbar and footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="text-center text-white backgroundMain lg:text-left">
-      <div class="bg-[#303030] p-6 text-center">
+    <footer className="text-center text-white backgroundMain lg:text-left">
+      <div className="bg-[#303030] p-6 text-center">
         <span>© 2024 Copyright : </span>
-        <a class="font-semibold">
+        <a className="font-semibold">
           Invest Right - All Rights Reserved
         </a>
       </div>
