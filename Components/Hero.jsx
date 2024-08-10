ca30e65ac9a0e9a1da85c385f276b9b408520edc
darshitbhalodilampros <darshitbhalodia@lampros.tech>
2024-08-10 09:59:29 +0530

    Added other tokens

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -54,20 +54,79 @@ const Hero = ({ titleData, createPrediction }) => {
   const pythPriceIdOptions = [
     {
       value:
+        "0x385f64d993f7b77d8182ed5003d97c60aa3361f3cecfe711544d2d59165e9bdf",
+      label: "OP",
+    },
+    {
+      value:
+        "0xd6835ad1f773de4a378115eb6824bd0c0e42d84d1c84d9750e853fb6b6c7794a",
+      label: "WLD",
+    },
+    {
+      value:
         "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
-      label: "BTC/USD",
+      label: "BTC",
     },
     {
       value:
         "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
-      label: "ETH/USD",
+      label: "ETH",
     },
     {
       value:
-        "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
-      label: "USDC/USD",
+        "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d",
+      label: "SOL",
     },
-    // Add more options as needed
+    {
+      value:
+        "0x2b9ab1e972a281585084148ba1389800799bd4be63b957507db1349314e47445",
+      label: "AAVE",
+    },
+    {
+      value:
+        "0x4a8e42861cabc5ecb50996f92e7cfa2bce3fd0a2423b0c44c9b423fb2bd25478",
+      label: "COMP",
+    },
+    {
+      value:
+        "0xdcef50dd0a4cd2dcc17e45df1676dcb336a11a61c69df7a0299b0150c672d25c",
+      label: "DOGE",
+    },
+    {
+      value:
+        "0x0781209c28fda797616212b7f94d77af3a01f3e94a5d421760aef020cf2bcb51",
+      label: "GALA",
+    },
+    { 
+      value:
+        "0x8ac0c70fff57e9aefdf5edf44b51d62c2d433653cbb2cf5cc06bb115af04d221",
+      label: "LINK",
+    },
+    {
+      value:
+        "0x5de33a9112c2b700b8d30b8a3402c103578ccfa2765696471cc672bd5cf6ac52",
+      label: "MATIC",
+    },
+    {
+      value:
+        "0xc415de8d2eba7db216527dff4b60e8f3a5311c740dadb233e13e12547e226750",
+      label: "NEAR",
+    },
+    {
+      value:
+        "0x67aed5a24fdad045475e7195c98a98aea119c763f272d4523f5bac93a4f33c2b",
+      label: "TRX",
+    },
+    {
+      value:
+        "0xec5d399846a9209f3fe5881d70aae9268c94339ff9817e8d18ff19fa05eea1c8",
+      label: "XRP",
+    },
+    {
+      value:
+        "0x78d185a741d07edb3412b09008b7c5cfb9bbbd7d568bf00ba737b456ba171501",
+      label: "UNI",
+    }
   ];
 
   const handleVerify = async (proof) => {
